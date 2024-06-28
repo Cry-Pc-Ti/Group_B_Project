52ce9f4b1f2105481dea38e637825f56a87fed2e
Cry_Pc_Ti <red.snp.and@gmail.com>
2024-06-28 15:59:22 +0900

    feat: ログイン画面を追加

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
 import streamlit as st
+from screen.login_screen import login_page
+
+
+def main():
+    st.set_page_config(
+        page_title="Streamlit App",
+        page_icon="static/img/icon.png",
+    )
+    login_page()
+
+
+if __name__ == "__main__":
+    main()
